88a9eb23df3c970fad84fc8d3917575a7e1422f8
SontaMana <144042181+SontaMana@users.noreply.github.com>
2023-09-23 17:36:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('My Parents new healthy Diner')
+streamlit.title('My Parents new healthy Diner..yaay')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
