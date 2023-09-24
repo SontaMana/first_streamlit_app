07b182c8220d534f25894138942d8e5db1b7b367
SontaMana <144042181+SontaMana@users.noreply.github.com>
2023-09-24 16:07:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
 import streamlit
 import pandas
 import snowflake.connector
-import requests
+#import requests
+
 streamlit.title('LAVENDER ORACLE VIBES')
 
 streamlit.header('Breakfast Menu')
@@ -28,6 +29,7 @@ def get_fruity_vice_data(this_fruit_choice):
    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
    return fruityvice_normalized
+   
 try:
    fruit_choice = streamlit.text_input('What fruit would you like information about?')
    if not fruit_choice:
@@ -35,10 +37,6 @@ try:
    else:
         back_from_function=get_fruity_vice_data(fruit_choice)
         streamlit.dataframe(back_from_function)
-
-
-
-
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
